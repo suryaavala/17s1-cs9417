3338b2a68bd65211a932a14c403c3b1227930d5f
Surya Avala <suryaavala.dev@gmail.com>
2017-06-06 13:48:10 +1000

    updated softmax

--- src/mnist_softmax.py
+++ src/mnist_softmax.py
@@ -1,35 +1,47 @@
+import sys
 from tensorflow.examples.tutorials.mnist import input_data
-
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 import tensorflow as tf
 
-x = tf.placeholder(tf.float32,[None, 784])
 
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+def main(_):
 
-y = tf.nn.softmax(tf.matmul(x, W)+b)
+    #Import data
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+    #Create Model
+    x = tf.placeholder(tf.float32, [None, 784])
+    W = tf.Variable(tf.zeros([784, 10]))
+    b = tf.Variable(tf.zeros([10]))
+    y = tf.matmul(x, W) + b
 
+    #Define loss and optimizer
+    y_ = tf.placeholder(tf.float32, [None, 10])
 
-y_ = tf.placeholder(tf.float32, [None, 10])
-
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y), reduction_indices=[1]))
-
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-sess = tf.InteractiveSession()
-
-tf.global_variables_initializer().run()
-
-for _ in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_:batch_ys})
+    #loss
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+    #optimising loss with GradientDescentOptimizer
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
 
 
-print(sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels}))
+    for _ in range(1000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+
+
+
+if __name__ == '__main__':
+    import time
+    start_time = time.time()
+
+    tf.app.run(main=main, argv=sys.argv)
