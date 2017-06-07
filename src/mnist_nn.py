1360b2332f1072bb5eb7ccf81ffa1e834f26e8d0
Surya Avala <suryaavala.dev@gmail.com>
2017-06-07 08:28:55 +0000

    removed initial softmax

--- src/mnist_nn.py
+++ src/mnist_nn.py
@@ -6,33 +6,33 @@ import tensorflow as tf
 
 x = tf.placeholder(tf.float32,[None, 784])
 
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+#W = tf.Variable(tf.zeros([784, 10]))
+#b = tf.Variable(tf.zeros([10]))
 
-y = tf.nn.softmax(tf.matmul(x, W)+b)
+#y = tf.nn.softmax(tf.matmul(x, W)+b)
 
 
 
 y_ = tf.placeholder(tf.float32, [None, 10])
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y), reduction_indices=[1]))
+#cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y), reduction_indices=[1]))
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+#train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess = tf.InteractiveSession()
 
-tf.global_variables_initializer().run()
+#tf.global_variables_initializer().run()
 
-for _ in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_:batch_ys})
-
-
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#for _ in range(1000):
+#    batch_xs, batch_ys = mnist.train.next_batch(100)
+#    sess.run(train_step, feed_dict={x: batch_xs, y_:batch_ys})
 
 
-print(sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels}))
+#correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_,1))
+#accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+#print(sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels}))
 
 
 def weight_variable(shape):
@@ -86,13 +86,13 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.global_variables_initializer())
-for i in range(20000):
+for i in range(30000):
   batch = mnist.train.next_batch(50)
   if i%100 == 0:
     train_accuracy = accuracy.eval(feed_dict={
         x:batch[0], y_: batch[1], keep_prob: 1.0})
     print("step %d, training accuracy %g"%(i, train_accuracy))
-  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.45})
 
 print("test accuracy %g"%accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
@@ -101,5 +101,5 @@ print("test accuracy %g"%accuracy.eval(feed_dict={
 
 #saving the nn
 saver = tf.train.Saver()
-save_path = saver.save(sess, "./model/model.ckpt")
+save_path = saver.save(sess, "./model75/model.ckpt")
 print("Model saved in file: %s" % save_path)
